3de07e63b5817da1de5866bad6dd61e8bc3e76f3
sabiqthottoly <64228146+sabiqthottoly@users.noreply.github.com>
2020-08-14 00:32:57 +0530

    Update PhoneNo.jsx

--- src/PhoneNo.jsx
+++ src/PhoneNo.jsx
@@ -8,76 +8,40 @@ export default class PhoneNo extends Component {
         super(props);
         this.state={
            input:null,
-           step:1,
-           otp:null,
            errors:{
                input:""
            }
         }
     }
     
-    //goto next step    
-    nextStep=()=>{
-        const{step}=this.state
-        this.setState({
-            step:step+1
-        });
-    }
-    //go back to previous step
-    pevStep=()=>{
-        const{step}=this.state
-        this.setState({
-            step:step-1
-        });
-    }
+
     
 
-    //handel input
-    handleChange = event => {
-        this.setState({ input: event.target.value });
-        let errors = this.state.errors;
-        this.setState({errors, [name]: value});
-      };
+    storeInput = (event) => {
+        event.preventDefault();
+        this.setState({
+            input:event.target.value,
+        });
+        console.log(this.state.input);
+    
+
+    };
   
   
  render() {
-     const{step}=this.state;
-     const{input,otp}=this.state;
-     const values ={input,otp}
-
-     switch(step){
-         case 1:
-            return(
-                <PhoneNumber
-                  nextStep={this.nextStep}
-                  handleChange={this.handleChange}
-                  values={values}/>
-            )
-      case 2:
-          return(
-               <OTP
-               nextStep={this.nextStep}
-                  handleChange={this.handleChange}
-                  values={values}/>
-
-          )
-
-          } 
-        
-
         return (
             
             <div className="main">
                 <div className="header">
                     <h1>Your Phone Number</h1>
-                    <h2>please confirm your phone number</h2>
+                    <h2>Please enter your  mobile number</h2>
                 </div>
 
                 <div className="phonenumber">
                     <input
                         className="input"
                         type="text"
-                        onChange={this.handleChange}
+                        onChange={this.storeInput}
                         value={this.state.input}
                         name="input"
                         maxLength="14"
@@ -89,7 +53,7 @@ export default class PhoneNo extends Component {
                     <Link to ="/otp" className="next-button" >NEXT</Link>
                 
                 </div>
-                <p>{this.state.input}</p>
+                
             </div>
             
         );
